47700489387c744528b6874b662da8b10c154a3c
John Connor <john_a_connor@icloud.com>
2015-02-27 18:38:52 -0500

    Whitelist support for RPC

--- src/rpc_manager.cpp
+++ src/rpc_manager.cpp
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2013-2015 John Connor (BM-NC49AxAjcqVcF5jNPu85Rb8MJ2d9JqZt)
  *
- * This file is part of coinpp.
+ * This file is part of vanillacoin.
  *
- * coinpp is free software: you can redistribute it and/or modify
+ * vanillacoin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License with
  * additional permissions to the one published by the Free Software
  * Foundation, either version 3 of the License, or (at your option)
@@ -160,6 +160,11 @@ void rpc_manager::handle_accept(
                 "RPC manager is dropping non-whitelisted connection from " <<
                 transport->socket().remote_endpoint() << "."
             );
+            
+            /**
+             * Stop the transport.
+             */
+            transport->stop();
         }
     }
 }
