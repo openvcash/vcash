523744a0c079e711ec49ca9bfda880f2421739a5
John Connor <john_a_connor@icloud.com>
2015-04-07 22:03:47 -0400

    disable lockfile on Android

--- src/stack_impl.cpp
+++ src/stack_impl.cpp
@@ -3632,7 +3632,8 @@ void stack_impl::load_wallet(
 
 void stack_impl::lock_file_or_exit()
 {
-#if (! defined _MSC_VER && ! defined __IPHONE_OS_VERSION_MAX_ALLOWED)
+#if (! defined _MSC_VER && ! defined __IPHONE_OS_VERSION_MAX_ALLOWED && \
+    ! defined __ANDROID__)
     static file f;
     
     if (
