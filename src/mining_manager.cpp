f3aec985276c341b53ee4fd1a0197d87e86af4dd
John Connor <john_a_connor@icloud.com>
2015-05-29 00:50:37 -0400

    use slightly larger stake interval for clients

--- src/mining_manager.cpp
+++ src/mining_manager.cpp
@@ -23,6 +23,7 @@
 
 #include <coin/block.hpp>
 #include <coin/globals.hpp>
+#include <coin/hash_scrypt.hpp>
 #include <coin/key_reserved.hpp>
 #include <coin/logger.hpp>
 #include <coin/mining.hpp>
@@ -281,6 +282,8 @@ const double & mining_manager::hashes_per_second() const
 
 void mining_manager::loop(const bool & is_proof_of_stake)
 {
+    void * buf_scrypt = scrypt_buffer_alloc();
+    
     key_reserved reserve_key(*globals::instance().wallet_main());
     
     std::uint32_t extra_nonce = 0;
@@ -327,6 +330,8 @@ void mining_manager::loop(const bool & is_proof_of_stake)
         
         if (blk == 0)
         {
+            free(buf_scrypt);
+            
             return;
         }
         
@@ -447,12 +452,18 @@ void mining_manager::loop(const bool & is_proof_of_stake)
             )
         {
             std::uint32_t hashes_done = 0;
-
+#if (defined USE_WHIRLPOOL && USE_WHIRLPOOL)
             auto nonce_found =
                 mining::scan_hash_whirlpool(&blk->header(), max_nonce,
                 hashes_done, result.digest(), &res_header
             );
-            
+#else
+            auto nonce_found = scanhash_scrypt(
+                &blk->header(), buf_scrypt,
+                max_nonce, hashes_done,
+                reinterpret_cast<std::uint32_t *> (result.digest()), &res_header
+            );
+#endif // USE_WHIRLPOOL
             /**
              * Check if we have found a solution.
              */
@@ -640,6 +651,8 @@ void mining_manager::loop(const bool & is_proof_of_stake)
     log_debug(
         "Mining manager thread " << std::this_thread::get_id() << " stopped."
     );
+    
+    free(buf_scrypt);
 }
 void mining_manager::pos_tick(const boost::system::error_code & ec)
 {
@@ -742,14 +755,14 @@ void mining_manager::pos_tick(const boost::system::error_code & ec)
         }
     
         /**
-         * Peers use a 60 second stake search interval and clients use 200
+         * Peers use a 60 second stake search interval and clients use 64
          * seconds.
          */
         auto interval =
             globals::instance().operation_mode() ==
-            protocol::operation_mode_peer ? 60 : 200
+            protocol::operation_mode_peer ? 60 : 64
         ;
-
+        
         timer_pos_.expires_from_now(std::chrono::seconds(interval));
     	timer_pos_.async_wait(std::bind(
 			&mining_manager::pos_tick, this, std::placeholders::_1)
