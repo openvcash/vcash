30f544476e1a05ed21d22693d8efa987d6206a7a
John Connor <john_a_connor@icloud.com>
2015-06-09 02:03:43 -0400

    integrating ecdhe into communications

--- database/src/block.cpp
+++ database/src/block.cpp
@@ -1,10 +1,20 @@
-//
-//  block.cpp
-//  database
-//
-//  Created by John Connor on 6/19/13.
-//  Copyright (c) 2013 John Connor. All rights reserved.
-//
+/*
+ * Copyright (c) 2008-2015 John Connor (BM-NC49AxAjcqVcF5jNPu85Rb8MJ2d9JqZt)
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <database/block.hpp>
 #include <database/logger.hpp>
