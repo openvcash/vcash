0fb00f57bdd6d56bcfe3441880bc84f26879133e
John Connor <john_a_connor@icloud.com>
2016-04-02 22:10:34 -0400

    added error macro to protocol header for 0.4.5 merge

--- include/coin/protocol.hpp
+++ include/coin/protocol.hpp
@@ -50,6 +50,8 @@ class zerotime_lock;
 class zerotime_question;
 class zerotime_vote;
 
+#error 0.4.5 merge in progress, please wait until it has been completed.
+
 namespace protocol {
 
         /**
