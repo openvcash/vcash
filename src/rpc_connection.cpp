594ac09e0a22c89e7987ee61b1a72091c2590965
John Connor <john_a_connor@icloud.com>
2016-07-02 16:54:26 -0400

    Fixed get block to return the underlying serialized size of the raw block.

--- src/rpc_connection.cpp
+++ src/rpc_connection.cpp
@@ -2298,8 +2298,18 @@ rpc_connection::json_rpc_response_t rpc_connection::json_getblock(
                 
                 tx.set_merkle_branch(&blk);
                 
+                /**
+                 * Allocate a data_buffer for encoding the block.
+                 */
+                data_buffer buffer_block;
+                
+                /**
+                 * Encode the block to obtain the serialized size.
+                 */
+                blk.encode(buffer_block);
+                
                 ret.result.put("confirmations", tx.get_depth_in_main_chain());
-                ret.result.put("size", blk.size());
+                ret.result.put("size", buffer_block.size());
                 ret.result.put("height", index->height());
                 ret.result.put("version", blk.header().version);
                 ret.result.put(
