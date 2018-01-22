ca3bd005cb1737ed0e2e8794729a9166c8f8d51c
sum01 <sum01@protonmail.com>
2018-01-22 14:43:37 -0500

    Simplify some comments and max version checking

--- cmake/modules/vcash_dependencies.cmake
+++ cmake/modules/vcash_dependencies.cmake
@@ -1,27 +1,25 @@
 # Dependency versions & find_package() commands in a small module for easier updating
 
-# TODO: Remove OPENSSL_MAX_VER and max version check when 1.1.x compatibility fix is merged to master branch
-# TODO: Remove BERKELEYDB_MAX_VER and max version check when Vcash is compatible with version > 6.2
+# TODO: Remove Boost max version check when https://github.com/openvcash/vcash/issues/38 fixed
+# TODO: Remove OpenSSL max version check when 1.1.x compatibility fix is merged to master branch
+# TODO: Remove BerkeleyDB max version check when Vcash is compatible with version > 6.2
+
+function(max_ver_allowed dep_name check_ver max_ver)
+	IF(${check_ver} VERSION_GREATER ${max_ver})
+		message(FATAL_ERROR "Detected ${dep_name} v${check_ver}, which isn't compatible! Maximum of v${max_ver} is compatible.")
+	ENDIF()
+endfunction()
 
 # ~~ Boost ~~
 find_package(Boost "1.54.0" COMPONENTS system REQUIRED)
 # v1.66.0 removed a lot of previously working (but deprecated) things
 # TODO: Remove when https://github.com/openvcash/vcash/issues/38 is fixed
-set(BOOST_MAX_VER "1.65.1")
-# Max version check
-IF(Boost_VERSION VERSION_GREATER ${BOOST_MAX_VER})
-	message(FATAL_ERROR "The detected Boost v${Boost_VERSION} isn't compatible! Maximum of v${BOOST_MAX_VER} is compatible")
-ENDIF()
+max_ver_allowed("Boost" "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}" "1.65.1")
 
 # ~~ OpenSSL ~~
 # Do not set the letter ("status") version for OpenSSL!
 find_package(OpenSSL "1.0.1" REQUIRED)
-set(OPENSSL_MAX_VER "1.0.2") # Used in max ver check
-
-# Max version check
-IF(OPENSSL_VERSION VERSION_GREATER ${OPENSSL_MAX_VER})
-  message(FATAL_ERROR "The detected OpenSSL v${OPENSSL_VERSION} isn't compatible! Maximum of v${OPENSSL_MAX_VER} is compatible.")
-ENDIF()
+max_ver_allowed("OpenSSL" ${OPENSSL_VERSION} "1.0.2")
 
 # Only libcoin needs Berkeley DB
 IF(BUILD_VCASH_DAEMON OR INSTALL_LIBCOIN)
@@ -33,14 +31,10 @@ IF(BUILD_VCASH_DAEMON OR INSTALL_LIBCOIN)
   ELSE()
     set(BERKELEYDB_MIN_VER "6.0.0")
   ENDIF()
-  set(BERKELEYDB_MAX_VER "6.1.36") # Last release ver before v6.2, which isn't compatible
   find_package(BerkeleyDB ${BERKELEYDB_MIN_VER} REQUIRED)
+	max_ver_allowed("BerkeleyDB" ${BERKELEYDB_VERSION} "6.1.36")
 
-  # Max version check
-  IF(BERKELEYDB_VERSION VERSION_GREATER "${BERKELEYDB_MAX_VER}")
-    message(FATAL_ERROR "The detected BerkeleyDB v${BERKELEYDB_VERSION} isn't compatible! Maximum of v${BERKELEYDB_MAX_VER} is compatible.")
-  # Throw a warning if the user has DB ver < 6 but continue building
-  ELSEIF(BERKELEYDB_VERSION VERSION_LESS "6.0.0")
+  IF(BERKELEYDB_VERSION VERSION_LESS "6.0.0")
     message(WARNING "Pre-existing wallet data is not backwards compatible with version v5 of Berkeley DB if it was originally built with v6. Read https://github.com/openvcash/vcash/wiki/Compile-With-Cmake#preamble--warning for more info.")
   ENDIF()
 ENDIF()
